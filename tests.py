56128a95e579f8d982f06e6e74b851c8ea929a48
Herbew 3L <herbew3l@gmail.com>
2022-04-16 21:30:29 +0700

    test import

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from flask_cicd.apps.homes.conftest import app
+from flask_cicd.apps.tests.conftest import app
 
 with app.test_client() as test_client:
         res = test_client.get('/login')
